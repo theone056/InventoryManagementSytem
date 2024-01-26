45a1059108c8246143f51106b4259b6c305b4720
THEONE\colon <colond06@gmail.com>
2024-01-26 10:07:48 +0800

    Added partial view for Summary Cards and added SalesController

---
+++ InventoryManagementSytem/Controllers/SalesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagementSytem.Controllers
+{
+    public class SalesController : Controller
+    {
+        [Route("Sales")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InventoryManagementSytem/Models/SummaryCardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManagementSytem.Models
+{
+    public class SummaryCardViewModel
+    {
+        public string Title { get; set; }
+        public int Value { get; set; }
+    }
+}
